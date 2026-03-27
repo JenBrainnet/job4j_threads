1c9e04bda32eb8d035e809471639413382c12c67
JenBrainnet <benebrak01@gmail.com>
2026-03-27 11:49:20 +0300

    9. ThreadLocal [#504912]

---
+++ src/main/java/ru/job4j/threads/threadlocal/FirstThread.java
@@ -0,0 +1,11 @@
+package ru.job4j.threads.threadlocal;
+
+public class FirstThread extends Thread {
+
+    @Override
+    public void run() {
+        ThreadLocalDemo.threadLocal.set("This is thread 1.");
+        System.out.println(ThreadLocalDemo.threadLocal.get());
+    }
+
+}
---
+++ src/main/java/ru/job4j/threads/threadlocal/ThreadLocalDemo.java
@@ -0,0 +1,18 @@
+package ru.job4j.threads.threadlocal;
+
+public class ThreadLocalDemo {
+
+    public static ThreadLocal<String> threadLocal = new ThreadLocal<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread first = new FirstThread();
+        Thread second = new SecondThread();
+        threadLocal.set("This is main thread.");
+        System.out.println(threadLocal.get());
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+    }
+
+}
