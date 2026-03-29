9d549c9dfaa4cf0fec4e8da8f1c646e224142f81
JenBrainnet <benebrak01@gmail.com>
2026-03-30 02:57:34 +0300

    0. What is atomicity [#6856]

---
+++ src/main/java/ru/job4j/synchronization/Count.java
@@ -0,0 +1,15 @@
+package ru.job4j.synchronization;
+
+public class Count {
+
+    private int value;
+
+    public void increment() {
+        value++;
+    }
+
+    public int get() {
+        return value;
+    }
+
+}
